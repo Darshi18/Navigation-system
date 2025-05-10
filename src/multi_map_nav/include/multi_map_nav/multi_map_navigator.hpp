88090ae6764035cc5583b482f821bf768902b7f5
Darshi Dwivedi <95619948+Darshi18@users.noreply.github.com>
2025-05-10 13:23:37 +0530

    Update multi_map_navigator.hpp

--- src/multi_map_nav/include/multi_map_nav/multi_map_navigator.hpp
+++ src/multi_map_nav/include/multi_map_nav/multi_map_navigator.hpp
@@ -10,8 +10,9 @@
 #include <utility>
 #include <memory>
 #include <thread>
-
-#include "nav2_msgs/srv/load_map.hpp"
+#include <vector>
+#include <nav2_msgs/srv/load_map.hpp>
+#include <nav2_msgs/action/navigate_to_pose.hpp>
 
 class MultiMapNavigator : public rclcpp::Node
 {
@@ -20,25 +21,30 @@ public:
   ~MultiMapNavigator();
 
   void send_goal(double x, double y, double theta, const std::string & target_map);
+  void init_clients();
   void switch_map(const std::string & map_name);
   void reset_amcl();
-  std::pair<double, double> get_wormhole_position(const std::string & map_name);
+  std::pair<double, double> get_wormhole_position(const std::string & map_name, const std::string & to_map);
 
 private:
   using MultiMapNavigate = multi_map_nav_interfaces::action::MultiMapNavigate;
   using GoalHandle = rclcpp_action::ServerGoalHandle<MultiMapNavigate>;
 
-  // Action Client
-  rclcpp_action::Client<MultiMapNavigate>::SharedPtr nav_client_;
-
   // Action Server
   rclcpp_action::Server<MultiMapNavigate>::SharedPtr action_server_;
 
+  // Action Client
+  rclcpp_action::Client<nav2_msgs::action::NavigateToPose>::SharedPtr nav_client_;
+
+  // AMCL pose subscriber
+  rclcpp::Subscription<geometry_msgs::msg::PoseWithCovarianceStamped>::SharedPtr amcl_pose_sub_;
+  geometry_msgs::msg::PoseWithCovarianceStamped current_amcl_pose_;
+
   // AMCL reset publisher
   rclcpp::Publisher<geometry_msgs::msg::PoseWithCovarianceStamped>::SharedPtr initialpose_pub_;
-  
-  //Map load client for switching maps
-  rclcpp::Client<nav2_msgs::srv::LoadMap>::SharedPtr map_load_client_;  
+
+  // Map load client
+  rclcpp::Client<nav2_msgs::srv::LoadMap>::SharedPtr map_load_client_;
 
   // SQLite DB
   sqlite3 * db_;
@@ -46,7 +52,11 @@ private:
   // Current map
   std::string current_map_;
 
-  // Action server callbacks
+  // Initialization
+  void init_action_server();
+  void init_database();
+
+  // Callbacks
   rclcpp_action::GoalResponse handle_goal(const rclcpp_action::GoalUUID & uuid,
                                           std::shared_ptr<const MultiMapNavigate::Goal> goal);
 
@@ -55,7 +65,8 @@ private:
   void handle_accepted(const std::shared_ptr<GoalHandle> goal_handle);
 
   void execute_navigation(const std::shared_ptr<GoalHandle> goal_handle);
+
+  void amcl_pose_callback(const geometry_msgs::msg::PoseWithCovarianceStamped::SharedPtr msg);
 };
 
 #endif  // MULTI_MAP_NAV__MULTI_MAP_NAVIGATOR_HPP_
-
